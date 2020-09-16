cacdac79e2f4dcb80fd534e1a7ba1fd148e4466e
mxxnkyung@naver.com <mxxnkyung@naver.com>
2020-09-16 16:26:35 +0900

    vs-code with react-introduction first commit &push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import TOC from './components/TOC';
+
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+import ReadContent from './components/ReadContent';
+
+import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.max_content_id = 3;
+
+    this.state = {
+      mode : null,
+      selected_content_id : null,
+      subject:{ title : "9OOD, REACT !", sub : "What's UP BB" },
+      welcome:{ title : "Welcome", desc : "HELLO, REACT !" },
+      contents:[
+        {id : 1, title : "HTML", desc : "HTML is for information"},
+        {id : 2, title : "CSS", desc : "CSS is for design"},
+        {id : 3, title : "Java Script", desc : "Java Script is for interactive"}
+      ]
+    };
+  }
+  getReadContent(){
+    var i = 0;
+
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+
+  getContent(){
+    var _title, _desc, _article, _content = null;
+    if(this.state.mode === "welcome"){
+
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+
+    } else if(this.state.mode === "read"){
+      _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if(this.state.mode === "create"){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push({
+        //   id : this.max_content_id, title : _title, desc : _desc
+        // })
+        // var _contents = this.state.contents.concat({
+        //   id : this.max_content_id, title : _title, desc : _desc
+        // })
+        var newContents = Array.from(this.state.contents);
+        newContents.push({id : this.max_content_id, title : _title, desc : _desc})
+        this.setState({
+          contents : newContents
+        })
+      
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === "update"){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+      
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id : _id, title: _title, desc: _desc};
+            break;
+          }
+          i = i + 1;
+        }
+        this.setState({
+          contents : _contents
+        })
+      
+      }.bind(this)}></UpdateContent>
+    } 
+    return _article;
+  }
+
+
+  render() {
+    // console.log('App Render');
+  
+    return (
+      <div className="App">
+        <Subject 
+            title={this.state.subject.title} 
+            sub={this.state.subject.sub}
+            onChangePage={function(){
+              this.setState({
+                mode : "welcome"
+              })
+            }.bind(this)}>
+        </Subject>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode : "read",
+            selected_content_id : Number(id)
+          });
+        }.bind(this)} 
+        data={this.state.contents}>
+        </TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode : _mode
+          });
+        }.bind(this)}>
+
+        </Control>
+        {this.getContent()}
+        
+     </div>
+    )
+  }
 }
 
 export default App;
